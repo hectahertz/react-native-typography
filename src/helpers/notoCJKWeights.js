d34c1e83cce7cdfa0d34d760fe0ec30288f9902b
Hector Garcia <hectahertz@gmail.com>
2017-12-11 18:12:49 +0100

    Add Tall & Dense weight helpers

---
+++ src/helpers/notoCJKWeights.js
@@ -0,0 +1,18 @@
+// Noto Sans CJK has many more steps defined but they're not shipped with Android yet.
+
+const notoCJKWeights = {
+  regular: {
+    includeFontPadding: false,
+    fontFamily: "sans-serif",
+    fontWeight: "normal",
+    backgroundColor: "transparent"
+  },
+  bold: {
+    includeFontPadding: false,
+    fontFamily: "sans-serif",
+    fontWeight: "bold",
+    backgroundColor: "transparent"
+  }
+};
+
+export default notoCJKWeights;
