bbac321e46745c5d990def880527489a77030526
hectahertz <hectahertz@gmail.com>
2017-12-29 12:52:54 +0100

    Add the 'web' platform to systemDenseWeights and systemTallWeights

--- src/helpers/systemDenseWeights.js
+++ src/helpers/systemDenseWeights.js
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 import sanFranciscoWeights from "../helpers/sanFranciscoWeights";
 import notoCJKWeights from "../helpers/notoCJKWeights";
+import webWeights from "../helpers/webWeights";
 
 // For now these weights are really restricted on Android, even though San Francisco
 // has many steps defined for some dense languages, Noto Sans CJK only has regular
@@ -12,23 +13,28 @@ import notoCJKWeights from "../helpers/notoCJKWeights";
 const systemDenseWeights = {
   thin: Platform.select({
     android: notoCJKWeights.regular,
-    ios: sanFranciscoWeights.thin
+    ios: sanFranciscoWeights.thin,
+    web: webWeights.thin
   }),
   light: Platform.select({
     android: notoCJKWeights.regular,
-    ios: sanFranciscoWeights.light
+    ios: sanFranciscoWeights.light,
+    web: webWeights.light
   }),
   regular: Platform.select({
     android: notoCJKWeights.regular,
-    ios: sanFranciscoWeights.regular
+    ios: sanFranciscoWeights.regular,
+    web: webWeights.regular
   }),
   semibold: Platform.select({
     android: notoCJKWeights.bold,
-    ios: sanFranciscoWeights.semibold
+    ios: sanFranciscoWeights.semibold,
+    web: webWeights.semibold
   }),
   bold: Platform.select({
     android: notoCJKWeights.bold,
-    ios: sanFranciscoWeights.bold
+    ios: sanFranciscoWeights.bold,
+    web: webWeights.bold
   })
 };
 
--- src/helpers/systemTallWeights.js
+++ src/helpers/systemTallWeights.js
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 import sanFranciscoWeights from "../helpers/sanFranciscoWeights";
 import notoTallWeights from "../helpers/notoTallWeights";
+import webWeights from "../helpers/webWeights";
 
 // San Francisco falls back from regular to bold starting from semibold for the scripts
 // for which it doesn't have so many weight steps, so let's do the same for Noto.
@@ -8,23 +9,28 @@ import notoTallWeights from "../helpers/notoTallWeights";
 const systemTallWeights = {
   thin: Platform.select({
     android: notoTallWeights.regular,
-    ios: sanFranciscoWeights.thin
+    ios: sanFranciscoWeights.thin,
+    web: webWeights.thin
   }),
   light: Platform.select({
     android: notoTallWeights.regular,
-    ios: sanFranciscoWeights.light
+    ios: sanFranciscoWeights.light,
+    web: webWeights.light
   }),
   regular: Platform.select({
     android: notoTallWeights.regular,
-    ios: sanFranciscoWeights.regular
+    ios: sanFranciscoWeights.regular,
+    web: webWeights.regular
   }),
   semibold: Platform.select({
     android: notoTallWeights.bold,
-    ios: sanFranciscoWeights.semibold
+    ios: sanFranciscoWeights.semibold,
+    web: webWeights.semibold
   }),
   bold: Platform.select({
     android: notoTallWeights.bold,
-    ios: sanFranciscoWeights.bold
+    ios: sanFranciscoWeights.bold,
+    web: webWeights.bold
   })
 };
 
