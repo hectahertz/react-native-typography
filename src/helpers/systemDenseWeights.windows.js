98aacb48a825e62b1e5e9c68fff920a3594b4239
Jeremy Pope <Jeremy.Pope@eLeadCRM.com>
2018-02-19 11:02:36 -0600

    add files need to get react-native-windows to work

---
+++ src/helpers/systemDenseWeights.windows.js
@@ -0,0 +1,11 @@
+import webWeights from "../helpers/webWeights";
+
+const systemDenseWeights = {
+  thin: webWeights.thin,
+  light: webWeights.light,
+  regular: webWeights.regular,
+  semibold: webWeights.semibold,
+  bold: webWeights.bold
+};
+
+export default systemDenseWeights;
---
+++ src/helpers/systemTallWeights.windows.js
@@ -0,0 +1,11 @@
+import webWeights from "../helpers/webWeights";
+
+const systemTallWeights = {
+  thin: webWeights.thin,
+  light: webWeights.light,
+  regular: webWeights.regular,
+  semibold: webWeights.semibold,
+  bold: webWeights.bold
+};
+
+export default systemTallWeights;
