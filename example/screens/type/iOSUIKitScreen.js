7dcc5b1ea3c7876cbac0046d330b183e786ef26a
hectahertz <hectahertz@gmail.com>
2018-08-04 20:04:16 +0100

    Fix fontSizes on the iOSUIKit collection image and example app

--- example/screens/type/iOSUIKitScreen.js
+++ example/screens/type/iOSUIKitScreen.js
@@ -9,134 +9,134 @@ const iOSUIKitScreen = ({ navigation }) => {
       blackStyles={[
         {
           style: iOSUIKit.largeTitleEmphasized,
-          text: "Bold 44",
+          text: "Bold 34",
           name: "largeTitleEmphasized"
         },
         {
           style: iOSUIKit.title3Emphasized,
-          text: "Semi-Bold 31",
+          text: "Semi-Bold 20",
           name: "title3Emphasized"
         },
         {
           style: iOSUIKit.title3,
-          text: "Regular 31",
+          text: "Regular 20",
           name: "title3"
         },
         {
           style: iOSUIKit.bodyEmphasized,
-          text: "Semi-Bold 28",
+          text: "Semi-Bold 17",
           name: "bodyEmphasized"
         },
         {
           style: iOSUIKit.body,
-          text: "Regular 28",
+          text: "Regular 17",
           name: "body"
         },
         {
           style: iOSUIKit.subheadEmphasized,
-          text: "Semi-Bold 25",
+          text: "Semi-Bold 15",
           name: "subheadEmphasized"
         },
         {
           style: iOSUIKit.subhead,
-          text: "Regular 25",
+          text: "Regular 15",
           name: "subhead"
         },
         {
           style: iOSUIKit.subheadShort,
-          text: "Regular 25 Short",
+          text: "Regular 15 Short",
           name: "subheadShort"
         },
         {
           style: iOSUIKit.callout,
-          text: "Regular 26",
+          text: "Regular 16",
           name: "callout"
         },
         {
           style: iOSUIKit.footnoteEmphasized,
-          text: "Semi-Bold 23",
+          text: "Semi-Bold 13",
           name: "footnoteEmphasized"
         },
         {
           style: iOSUIKit.footnote,
-          text: "Regular 23",
+          text: "Regular 13",
           name: "footnote"
         },
         {
           style: iOSUIKit.caption2Emphasized,
-          text: "Semi-Bold 20",
+          text: "Semi-Bold 11",
           name: "caption2Emphasized"
         },
         {
           style: iOSUIKit.caption2,
-          text: "Regular 20",
+          text: "Regular 11",
           name: "caption2"
         }
       ]}
       whiteStyles={[
         {
           style: iOSUIKit.largeTitleEmphasizedWhite,
-          text: "Bold 44",
+          text: "Bold 34",
           name: "largeTitleEmphasizedWhite"
         },
         {
           style: iOSUIKit.title3EmphasizedWhite,
-          text: "Semi-Bold 31",
+          text: "Semi-Bold 20",
           name: "title3EmphasizedWhite"
         },
         {
           style: iOSUIKit.title3White,
-          text: "Regular 31",
+          text: "Regular 20",
           name: "title3White"
         },
         {
           style: iOSUIKit.bodyEmphasizedWhite,
-          text: "Semi-Bold 28",
+          text: "Semi-Bold 17",
           name: "bodyEmphasizedWhite"
         },
         {
           style: iOSUIKit.bodyWhite,
-          text: "Regular 28",
+          text: "Regular 17",
           name: "bodyWhite"
         },
         {
           style: iOSUIKit.subheadEmphasizedWhite,
-          text: "Semi-Bold 25",
+          text: "Semi-Bold 15",
           name: "subheadEmphasizedWhite"
         },
         {
           style: iOSUIKit.subheadWhite,
-          text: "Regular 25",
+          text: "Regular 15",
           name: "subheadWhite"
         },
         {
           style: iOSUIKit.subheadShortWhite,
-          text: "Regular 25 Short",
+          text: "Regular 15 Short",
           name: "subheadShortWhite"
         },
         {
           style: iOSUIKit.calloutWhite,
-          text: "Regular 26",
+          text: "Regular 16",
           name: "calloutWhite"
         },
         {
           style: iOSUIKit.footnoteEmphasizedWhite,
-          text: "Semi-Bold 23",
+          text: "Semi-Bold 13",
           name: "footnoteEmphasizedWhite"
         },
         {
           style: iOSUIKit.footnoteWhite,
-          text: "Regular 23",
+          text: "Regular 13",
           name: "footnoteWhite"
         },
         {
           style: iOSUIKit.caption2EmphasizedWhite,
-          text: "Semi-Bold 20",
+          text: "Semi-Bold 11",
           name: "caption2EmphasizedWhite"
         },
         {
           style: iOSUIKit.caption2White,
-          text: "Regular 20",
+          text: "Regular 11",
           name: "caption2White"
         }
       ]}
