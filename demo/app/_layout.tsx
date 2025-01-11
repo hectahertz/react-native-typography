b5a7a0ae82c27afc9b3c121f3579ecc41eb811d1
Hector Garcia <hectahertz@gmail.com>
2025-01-11 13:03:20 +0000

    Add Drawer

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Home',
+            title: 'Screen title',
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
